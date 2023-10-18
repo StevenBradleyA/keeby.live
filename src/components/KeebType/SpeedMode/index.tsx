3d82a351db9be5cac6d9aef55ab13cc216da8113
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-10-17 19:47:04 -0700

    added logic for pressing spacebar 🏹

--- src/components/KeebType/SpeedMode/index.tsx
+++ src/components/KeebType/SpeedMode/index.tsx
@@ -9,11 +9,31 @@ export default function SpeedMode({ gameLength }: SpeedModeProps) {
     const [prompt, setPrompt] = useState<string[]>([]);
     const [totalUserInput, setTotalUserInput] = useState<string>("");
     const [userInput, setUserInput] = useState<string>("");
+    const [activeWordIndex, setActiveWordIndex] = useState<number>(0);
+
+    console.log("userInput", userInput);
+    console.log("activeWordIndex", activeWordIndex);
+    console.log("totalInput", totalUserInput);
+
+    const handleInputChange = (inputValue: string) => {
+        setUserInput(inputValue);
+    };
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === " ") {
+            // Add userInput to totalUserInput and clear userInput
+            setTotalUserInput(totalUserInput + userInput + " ");
+            setUserInput("");
+            // Increase activeWordIndex by one
+            setActiveWordIndex(activeWordIndex + 1);
+            // Prevent the space key from being input into the text field
+            e.preventDefault();
+        }
+    };
 
     return (
-        <>
+        <div className="flex flex-col">
             <SentenceGenerator gameLength={gameLength} setPrompt={setPrompt} />
-            <div className="flex w-2/3 justify-center gap-2 text-gray-400">
+            <div className="flex w-2/3 gap-2 text-gray-400">
                 {prompt.map((word, index) => (
                     <div className="flex" key={index}>
                         {word.split("").map((letter, letterIndex) => (
@@ -24,6 +44,14 @@ export default function SpeedMode({ gameLength }: SpeedModeProps) {
                     </div>
                 ))}
             </div>
-        </>
+            <div>
+                <input
+                    value={userInput}
+                    onChange={(e) => handleInputChange(e.target.value)}
+                    onKeyDown={(e) => handleKeyDown(e)}
+                    className="w-full bg-dark text-red-600 "
+                />
+            </div>
+        </div>
     );
 }
