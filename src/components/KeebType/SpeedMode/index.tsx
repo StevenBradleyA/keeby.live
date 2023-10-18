c3791268526be0c0f3661e6fa11faf79cc028ccb
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-10-17 21:48:09 -0700

    ✈️ added more logic to speed mode ✈️

--- src/components/KeebType/SpeedMode/index.tsx
+++ src/components/KeebType/SpeedMode/index.tsx
@@ -10,14 +10,19 @@ export default function SpeedMode({ gameLength }: SpeedModeProps) {
     const [totalUserInput, setTotalUserInput] = useState<string>("");
     const [userInput, setUserInput] = useState<string>("");
     const [activeWordIndex, setActiveWordIndex] = useState<number>(0);
+    const [wordStatus, setWordStatus] = useState<boolean[]>(
+        new Array(prompt.length).fill(false)
+    );
 
-    console.log("userInput", userInput);
-    console.log("activeWordIndex", activeWordIndex);
-    console.log("totalInput", totalUserInput);
+    // console.log("userInput", userInput);
+    // console.log("activeWordIndex", activeWordIndex);
+    // console.log("totalInput", totalUserInput);
 
     const handleInputChange = (inputValue: string) => {
         setUserInput(inputValue);
+       
     };
+
     const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === " ") {
             // Add userInput to totalUserInput and clear userInput
@@ -29,18 +34,39 @@ export default function SpeedMode({ gameLength }: SpeedModeProps) {
             e.preventDefault();
         }
     };
+    // incorrect
+    // incorrect extra
 
     return (
         <div className="flex flex-col">
             <SentenceGenerator gameLength={gameLength} setPrompt={setPrompt} />
-            <div className="flex w-2/3 gap-2 text-gray-400">
+            <div className={`flex w-2/3 gap-2 text-gray-400 `}>
                 {prompt.map((word, index) => (
-                    <div className="flex" key={index}>
-                        {word.split("").map((letter, letterIndex) => (
-                            <div className="correct" key={letterIndex}>
-                                {letter}
-                            </div>
-                        ))}
+                    <div
+                        className={`flex ${
+                            index === activeWordIndex ? "active" : "word"
+                        }`}
+                        key={index}
+                    >
+                        {word.split("").map((letter, letterIndex) => {
+                            const isCorrect = userInput[letterIndex] === letter;
+                            const isWordCorrect = wordStatus[index];
+
+                            return (
+                                <div
+                                    className={`${
+                                        isCorrect
+                                            ? isWordCorrect
+                                                ? "correct"
+                                                : "correct-typing"
+                                            : ""
+                                    }`}
+                                    key={letterIndex}
+                                >
+                                    {letter}
+                                </div>
+                            );
+                        })}
                     </div>
                 ))}
             </div>
