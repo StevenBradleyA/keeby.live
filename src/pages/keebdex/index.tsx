f45ce206c3df0b356d937e82161502f5e1a3beb4
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-10-23 18:01:16 -0700

    👕 added styling to keeb dex 👕

--- src/pages/keebdex/index.tsx
+++ src/pages/keebdex/index.tsx
@@ -3,57 +3,60 @@ import { motion } from "framer-motion";
 import TitleScripts from "~/components/TitleScripts";
 
 export default function KeebDex() {
+    // do a different gif than the matrix and make it transparent
+
     return (
         <>
             <TitleScripts page={"keebdex"} />
-            <div className="z-30 flex flex-col gap-5  rounded-3xl bg-keebyGray px-20 py-10">
+            <div className="z-30 flex flex-col gap-5 rounded-3xl  bg-keebyGray px-20 py-10 text-green-500">
                 <motion.button
-                    className="button-hover-effect  rounded-2xl bg-black px-6 py-4 "
+                    className="keebdex-hover-effect rounded-2xl  bg-black  px-6 py-4 "
                     whileHover={{ scale: 1.1 }}
                     whileTap={{ scale: 0.95 }}
                 >
-                    {" "}
-                    How Do Auctions Work?{" "}
+                    How Do Auctions Work?
                 </motion.button>
                 <motion.button
-                    className="button-hover-effect  rounded-2xl bg-black px-6 py-4 "
+                    className="keebdex-hover-effect  rounded-2xl bg-black px-6 py-4 "
                     whileHover={{ scale: 1.1 }}
                     whileTap={{ scale: 0.95 }}
                 >
-                    {" "}
-                    How to Prevent Scams?{" "}
+                    How to Prevent Scams?
                 </motion.button>
                 <motion.button
-                    className="button-hover-effect  rounded-2xl bg-black px-6 py-4 "
+                    className="keebdex-hover-effect  rounded-2xl bg-black px-6 py-4 "
                     whileHover={{ scale: 1.1 }}
                     whileTap={{ scale: 0.95 }}
                 >
-                    {" "}
-                    Are There Returns?{" "}
+                    Are There Returns?
                 </motion.button>
                 <motion.button
-                    className="button-hover-effect  rounded-2xl bg-black px-6 py-4 "
+                    className="keebdex-hover-effect  rounded-2xl bg-black px-6 py-4 "
                     whileHover={{ scale: 1.1 }}
                     whileTap={{ scale: 0.95 }}
                 >
-                    {" "}
-                    What Is KEEBY?{" "}
+                    What Is KEEBY?
                 </motion.button>
                 <motion.button
-                    className="button-hover-effect  rounded-2xl bg-black px-6 py-4 "
+                    className="keebdex-hover-effect  rounded-2xl bg-black px-6 py-4 "
                     whileHover={{ scale: 1.1 }}
                     whileTap={{ scale: 0.95 }}
                 >
-                    {" "}
-                    Why Are We Still Here Just To Suffer?{" "}
+                    Why Are We Still Here Just To Suffer?
                 </motion.button>
                 <motion.button
-                    className="button-hover-effect  rounded-2xl bg-black px-6 py-4"
+                    className="keebdex-hover-effect  rounded-2xl bg-black px-6 py-4"
                     whileHover={{ scale: 1.1 }}
                     whileTap={{ scale: 0.95 }}
                 >
-                    {" "}
-                    Tactiles or Linears?{" "}
+                    Tactiles or Linears?
+                </motion.button>
+                <motion.button
+                    className="keebdex-hover-effect  rounded-2xl bg-black px-6 py-4"
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.95 }}
+                >
+                    Can I support Keeby?
                 </motion.button>
             </div>
         </>
