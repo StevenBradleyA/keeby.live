d05b7a385ec8be5d734c13b901f9baa782dd5750
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-10-25 11:14:57 -0700

    created new update user route for the profile plus 💥

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -14,4 +14,43 @@ export const userRouter = createTRPCRouter({
             });
             return Boolean(user);
         }),
+
+    updateNewUser: protectedProcedure
+        .input(
+            z.object({
+                userId: z.string(),
+                username: z.string(),
+                images: z
+                    .array(
+                        z.object({
+                            link: z.string(),
+                        })
+                    )
+                    .optional(),
+            })
+        )
+        .mutation(async ({ input, ctx }) => {
+            const { userId, username, images } = input;
+            if (ctx.session.user.id === userId) {
+                const updateData: {
+                    username: string;
+                    hasProfile?: boolean;
+                    profile?: string;
+                } = {
+                    username,
+                    hasProfile: true,
+                };
+
+                if (images && images.length > 0) {
+                    updateData.profile = images[0]?.link;
+                }
+
+                return await ctx.prisma.user.update({
+                    where: { id: ctx.session.user.id },
+                    data: updateData,
+                });
+            }
+
+            throw new Error("Invalid userId");
+        }),
 });
