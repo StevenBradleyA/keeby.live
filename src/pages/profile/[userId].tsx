1378d4fa5637ae7f386ea933645ed666aa19384b
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-10-26 11:06:46 -0700

    ✈️ added new ideas to profile page ✈️

--- src/pages/profile/[userId].tsx
+++ src/pages/profile/[userId].tsx
@@ -21,27 +21,31 @@ export default function UserProfile() {
             {isRetro && <div className="retro-scanlines"></div>}
 
             <TitleScripts page="profile" />
-            <div className="mb-10 flex">
-                <div className="p-5 outline outline-1 outline-green-500">
+            <div className="mb-10 flex w-full justify-center gap-10">
+                <div className="flex flex-col">
                     {sessionData && sessionData.user.profile ? (
                         <Image
                             alt="profile"
                             src={sessionData?.user.profile}
-                            width={250}
-                            height={250}
+                            width={300}
+                            height={300}
                         />
                     ) : (
                         <Image
                             alt="profile"
                             src={keebo}
-                            width={200}
-                            height={200}
+                            width={300}
+                            height={300}
                         />
                     )}
+                    <div className=" mt-5 p-5 outline outline-1 outline-green-500">
+                        {sessionData?.user.username}
+                    </div>
+                    <div>tag selection dropdown here</div>
                 </div>
 
-                <div className="flex flex-col">
-                    <div className=" h-[300px] w-[300px]  outline outline-1 outline-green-500">
+                <div className="flex w-1/3 flex-col">
+                    <div className="h-72 outline outline-1 outline-green-500 ">
                         <Canvas
                             className="h-full w-full cursor-pointer"
                             onClick={() => setIsRetro(!isRetro)}
@@ -51,23 +55,31 @@ export default function UserProfile() {
                             <RotatingSphere />
                         </Canvas>
                     </div>
-                    <div>hello moving text</div>
+                    <div className=" p-10 outline outline-1 outline-green-500">
+                        hello moving text
+                    </div>
                 </div>
             </div>
 
-            <div className="p-5 outline outline-1 outline-green-500">
-                {sessionData?.user.username}
+            <div className="flex gap-5">
+                <button>KeebType</button>
+                <button>KeebShop</button>
+                <button>KeebShare</button>
             </div>
+
             <button className="rounded-2xl bg-green-600 px-6 py-2">
                 isVerifiedSeller
             </button>
             <button className="rounded-2xl bg-red-600 px-6 py-2">
                 or show dis Register to Sell a KEEB
             </button>
-            <div>
-                Tag selection like rocketleague ---other users will only see tag
-                selected ---
-            </div>
+            <button className="rounded-2xl bg-green-600 px-6 py-2">
+                My Listings
+            </button>
+            <button className="rounded-2xl bg-green-600 px-6 py-2">
+                My Posts
+            </button>
+            <div>graph with wpm date and keeb etc</div>
             <div>Keeb Information and highest avg wpm</div>
             <div>user only -- show 10 highest wpm</div>
             <div>Rank information</div>
