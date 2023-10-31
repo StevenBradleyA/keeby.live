48b7de72e9a11d517a946b636450128626fee801
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-10-31 14:11:33 -0700

    🥇 updated db to include listings and likes for comments 🥇

---
+++ src/pages/verification/index.tsx
@@ -0,0 +1,28 @@
+export default function VerifyUser() {
+
+    //  I wanted to create a safe fun place to buy and sell keyboards
+    //  
+    return (
+        <>
+            <div>going to want to verify email</div>
+            <div>going to want to verify payment via stripe</div>
+            <div>
+                We don't save or interact with your payment data its all securly
+                stored via stripe{" "}
+            </div>
+
+            <div>Before Listing a keeb you must know...</div>
+            <button>How we prevent scams</button>
+            <button>
+                How much does it cost. Each listing costs $.50 and we take a 5%
+                fee from the final price.
+            </button>
+            <div>How do I stay safe as a seller? </div>
+            <div>
+                --page that explains taking a video of your keyboard working
+                before sale will protect you from buyers claiming non working
+                keyboards.{" "}
+            </div>
+        </>
+    );
+}
