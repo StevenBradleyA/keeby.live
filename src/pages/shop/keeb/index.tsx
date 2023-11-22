271f4868c07f9a0eb6d25c1dce13fffc04d898f3
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-11-22 12:36:19 -0600

    💣 creating new listing page for shop 💣

---
+++ src/pages/shop/keeb/index.tsx
@@ -0,0 +1,11 @@
+interface ListingPageProps {
+    listingId: string;
+}
+
+export default function ListingPage({ listingId }: ListingPageProps) {
+    return (
+        <>
+            <div></div>
+        </>
+    );
+}
