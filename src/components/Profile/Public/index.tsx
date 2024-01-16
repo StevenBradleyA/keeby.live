a667a326ed058037bf343939ec83141dcedf8a2e
StevenbradleyA <andersonstevenbradley@gmail.com>
2024-01-15 18:06:12 -0800

    🔥 created new route to get public user info  🔥

---
+++ src/components/Profile/Public/index.tsx
@@ -0,0 +1,13 @@
+interface PublicProfileProps {
+    userId: string;
+}
+
+export default function PublicProfile({ userId }: PublicProfileProps) {
+    // going to take in a userID and query that users info
+
+    return (
+        <>
+            <div></div>
+        </>
+    );
+}
