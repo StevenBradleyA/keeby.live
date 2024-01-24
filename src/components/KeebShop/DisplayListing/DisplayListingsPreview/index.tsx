b811a3b9eefde81aa107da2f6f0a50b5a96f9472
StevenbradleyA <andersonstevenbradley@gmail.com>
2024-01-24 13:38:20 -0800

    🔥 added stats on listing preview hover 🔥

--- src/components/KeebShop/DisplayListing/DisplayListingsPreview/index.tsx
+++ src/components/KeebShop/DisplayListing/DisplayListingsPreview/index.tsx
@@ -3,6 +3,8 @@ import type { Listing } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 import LoadingSpinner from "~/components/Loading";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 interface EachListingCardProps {
     keeb: Listing;
@@ -10,6 +12,8 @@ interface EachListingCardProps {
 }
 
 export default function EachListingCardPreview({ keeb }: EachListingCardProps) {
+    const [isPhotoHover, setIsPhotoHover] = useState<boolean>(false);
+
     const { data: previewImage, isLoading } =
         api.image.getAllByResourceId.useQuery({
             resourceType: "LISTINGPREVIEW",
@@ -29,7 +33,11 @@ export default function EachListingCardPreview({ keeb }: EachListingCardProps) {
                 }}
             >
                 {previewImage && previewImage[0] && previewImage[0].link && (
-                    <div className=" image-container h-72 w-96  cursor-pointer rounded-2xl ">
+                    <div
+                        className="listing-preview-hover-effect relative h-72 w-96  cursor-pointer overflow-hidden rounded-2xl "
+                        onMouseEnter={() => setIsPhotoHover(true)}
+                        onMouseLeave={() => setIsPhotoHover(false)}
+                    >
                         <Image
                             alt="preview"
                             src={previewImage[0].link}
@@ -38,6 +46,16 @@ export default function EachListingCardPreview({ keeb }: EachListingCardProps) {
                             className={`h-full w-full object-cover`}
                         />
                         <div></div>
+
+                        <div
+                            className={`${
+                                isPhotoHover
+                                    ? "show-listing-hover-content"
+                                    : "hide-listing-hover-content"
+                            }   absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2`}
+                        >
+                            <div className="text-green-500  ">{`Check it out`}</div>
+                        </div>
                     </div>
                 )}
                 <div className="text-darkGray">{keeb.title}</div>
