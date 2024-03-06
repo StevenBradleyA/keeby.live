9c2d5a74e6ce3765e2309d0600e48704a76a551c
StevenbradleyA <andersonstevenbradley@gmail.com>
2024-03-06 11:06:11 -0800

    added new admin transactions page ⏯

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -9,9 +9,9 @@ export default function Admin() {
     const accessDenied = !session || !session.user.isAdmin;
 
     return !accessDenied ? (
-        <div className="h-full  w-1/2  ">
-            <div className="relative flex justify-center ">
-                <div className="relative flex w-full justify-center overflow-hidden rounded-xl bg-black bg-opacity-50 p-5">
+        <div className="h-full  w-full flex flex-col items-center ">
+            <div className="relative flex justify-center w-full">
+                <div className=" w-1/2 relative flex justify-center overflow-hidden rounded-xl bg-black bg-opacity-50 p-5">
                     <Image
                         src={hackerman}
                         alt="admin-logo"
@@ -29,7 +29,7 @@ export default function Admin() {
                     </div>
                 </div>
             </div>
-            <div className="mt-20 flex w-full justify-evenly">
+            <div className="mt-20 flex w-3/4 justify-evenly">
                 <Link
                     href="/admin/listings"
                     className="flex flex-col items-center"
@@ -67,6 +67,19 @@ export default function Admin() {
                     />
                     <h3 className="font-titillium text-xl text-failure">
                         RANKS
+                    </h3>
+                </Link>
+                <Link
+                    href="/admin/transactions"
+                    className="flex flex-col items-center"
+                >
+                    <Image
+                        src={errorComputer}
+                        alt="admin-logo"
+                        className="png-red w-24 "
+                    />
+                    <h3 className="font-titillium text-xl text-failure">
+                        TRANSACTIONS
                     </h3>
                 </Link>
                 <Link
